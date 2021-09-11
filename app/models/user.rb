ec657bfdc8a9fa33ca6ca607ef53e0c7acfae689
Mikoto882 <hisashi.doraguniru@gmail.com>
2021-09-11 23:54:42 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,16 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  with_options presence: true do
+    validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze, message: "には英字と数字の両方を含めて設定してください"}
+    validates :nickname
+    validates :old, format: {with: /\A(?=.*?[\d])[\d]+\z/i.freeze, message: "には数字のみ入力して下さい"}
+    validates :grade_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :birthday
+  end
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :grade
 
-  validates :grade_id, numericality: { other_than: 1 }
 end
